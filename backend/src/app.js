1102cf05784d7c2ce91f022f9f20fd3834ea4214
TonyReignsT <antonyochieng00@gmail.com>
2026-05-02 12:36:40 +0300

    Initial commit

---
+++ backend/src/app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+require('dotenv').config()
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("We are live...")
+})
+
+
+module.exports = app
+
